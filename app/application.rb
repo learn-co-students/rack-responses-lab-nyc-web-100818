ac28a924cc6afa564e0eab2b7a863297a8f01169
magic8baller <lturis@gmail.com>
2018-10-27 16:09:06 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,18 @@
+class Application
+
+  #runs whenever request received
+  def call(env)
+    resp = Rack::Response.new
+
+    time_now = Time.now.hour
+
+    if time_now < 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+
+    resp.finish
+  end
+
+end
